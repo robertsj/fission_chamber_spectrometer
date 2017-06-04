f846ebdbe37d7e8fbc3010ba100e964aed5d02ef
Jeremy Roberts <jaroberts@ksu.edu>
2017-06-04 11:28:23 -0500

    Added unfolding functions

--- code/driver.py
+++ code/driver.py
@@ -24,19 +24,15 @@ def generate_all_responses() :
     triga = Flux(2.0, 600.0) 
     resp = generate_responses(isos, triga.evaluate, struct=struct, name='tg0_625_triga_resp.p', overwrite=True)
   
-    
     # 32 group
-    struct = 'phoenix25'
+    struct = 'lwr32'
     fun = lambda x: 0*x+1
-    resp = generate_responses(isos, fun, struct=struct, name='phoenix25_flat_resp.p', overwrite=True)
+    resp = generate_responses(isos, fun, struct=struct, name='lwr32_flat_resp.p', overwrite=True)
     pwr = Flux(7.0, 600.0)
-    resp = generate_responses(isos, pwr.evaluate, struct=struct, name='phoenix25_pwr_resp.p', overwrite=True)
+    resp = generate_responses(isos, pwr.evaluate, struct=struct, name='lwr32_pwr_resp.p', overwrite=True)
     triga = Flux(2.0, 600.0) 
-    resp = generate_responses(isos, triga.evaluate, struct=struct, name='phoenix25_triga_resp.p', overwrite=True)
+    resp = generate_responses(isos, triga.evaluate, struct=struct, name='lwr32_triga_resp.p', overwrite=True)
   
-    
-    
-    
     # 69 group
     struct = 'wims69'
     fun = lambda x: 0*x+1
@@ -46,26 +42,24 @@ def generate_all_responses() :
     triga = Flux(2.0, 600.0) 
     resp = generate_responses(isos, triga.evaluate, struct=struct, name='wims69_triga_resp.p', overwrite=True)
   
-    
     # 238 group
-    struct = 'scale238'
+    struct = 'scale252'
     fun = lambda x: 0*x+1
-    resp = generate_responses(isos, fun, struct=struct, name='wims238_flat_resp.p', overwrite=True)
+    resp = generate_responses(isos, fun, struct=struct, name='scale252_flat_resp.p', overwrite=True)
     pwr = Flux(7.0, 600.0)
-    resp = generate_responses(isos, pwr.evaluate, struct=struct, name='wims238_pwr_resp.p', overwrite=True)
+    resp = generate_responses(isos, pwr.evaluate, struct=struct, name='scale252_pwr_resp.p', overwrite=True)
     triga = Flux(2.0, 600.0) 
-    resp = generate_responses(isos, triga.evaluate, struct=struct, name='wims238_triga_resp.p', overwrite=True)
-  
-    
+    resp = generate_responses(isos, triga.evaluate, struct=struct, name='scale252_triga_resp.p', overwrite=True)
+ 
     
 if __name__ == "__main__" :
     
-    generate_all_responses()
+    #generate_all_responses()
     
-    resp1 = generate_responses(isos, None, name='tg0_625_flat_resp.p', overwrite=False)
-    resp2 = generate_responses(isos, None, name='tg0_625_pwr_resp.p', overwrite=False)
+    resp1 = generate_responses(isos, None, name='scale252_flat_resp.p', overwrite=False)
+    resp2 = generate_responses(isos, None, name='scale252_pwr_resp.p', overwrite=False)
 
     for iso in isos:
         x, y = plot_multigroup_data(resp1['eb'],  resp2[iso])
         plt.loglog(x, y, ls='-',color=isos_colors[iso])
-        #plt.legend(isos)
+        plt.legend(isos_str.values())
--- code/response.py
+++ code/response.py
@@ -16,23 +16,21 @@ def generate_responses(isos, flux, struct='wims69', lower=1e-5, upper=2e7,
     responses['eb'] = eb
     responses['phi'] = sp.zeros(len(eb)-1)
     for i in range(len(eb)-1) :
-        #responses['phi'][i] = quad(flux, eb[i+1], eb[i], limit=150)[0]
         E = sp.logspace(sp.log10(eb[i+1]), sp.log10(eb[i]), 1e4)
         x = trapz(flux(E), E)
         print("->",i,eb[i+1], eb[i], x)
         responses['phi'][i] = trapz(flux(E), E)
         
     interps = get_cross_section_interps(isos)
+    responses['response'] = {}
     for iso in isos :
         print("...response for iso ", iso)
-        responses[iso] = sp.zeros(len(eb)-1)
+        responses['response'][iso] = sp.zeros(len(eb)-1)
         fun = lambda x: flux(x)*interps[iso](x)
         for i in range(len(eb)-1) :
-            #print(' ----', i)
-            #top = quad(fun, eb[i+1], eb[i], limit=150)[0]
             E = sp.logspace(sp.log10(eb[i+1]), sp.log10(eb[i]), 1e4)
             top = trapz(fun(E), E)
-            responses[iso][i] = top/responses['phi'][i]
+            responses['response'][iso][i] = top/responses['phi'][i]
     pickle.dump(responses, open(name, 'wb'))
     return responses
     
@@ -42,7 +40,7 @@ if __name__ == "__main__" :
     from multigroup_utilities import plot_multigroup_data
     struct = 'wims69'
     phi = Flux(7.0, 600.0)
-    resp = generate_responses(['u235'], phi.evaluate, name='test_resp.p', overwrite=True)
-    for iso in ['u235']:#isos:
-        x, y = plot_multigroup_data(resp['eb'], resp[iso])
+    resp = generate_responses(isos, phi.evaluate, name='test_resp.p', overwrite=True)
+    for iso in isos:
+        x, y = plot_multigroup_data(resp['eb'], resp['response'][iso])
         plt.loglog(x, y, ls='-')
