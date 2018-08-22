1543e050652df2a54ecd0c964b852676c81918bc
John Boyington <mjcb@ksu.edu>
2018-08-22 11:49:23 -0500

    now stores unfolded umg data in pickle file; initialized script for processing umg data

--- code/unfold_umg.py
+++ code/unfold_umg.py
@@ -2,20 +2,17 @@ from unfolding_tools import Unfolding
 import pickle
 import scipy as sp
 import numpy as np
-from scipy.optimize import fmin_cobyla, minimize
-from master_data import directory
 from unfold import integral_response
 from flux_spectrum import Flux
-from master_data import isos, img_directory, data_path, plot_path
+from master_data import isos, data_path, plot_path
 from response import generate_responses
-from multigroup_utilities import energy_groups, plot_multigroup_data
+from multigroup_utilities import energy_groups
 from spectrum import Spectrum
 import matplotlib.pyplot as plt
 import os
 import shutil
 from scipy.integrate import trapz
 from flux import select_flux_spectrum
-
 
 
 def unfold_umg(R, RF, ds, ts, eb, isos, name, program):
@@ -28,7 +25,7 @@ def unfold_umg(R, RF, ds, ts, eb, isos, name, program):
         rfs[i] = RF[iso]
         names = ['{:8.8}'.format(iso), '{:16.16}'.format(iso)]
         ids.append(names)
-        
+
     U = Unfolding()
     U.setSphereIDs(ids)
     U.setSphereSizes(np.ones(len(isos)))
@@ -37,55 +34,55 @@ def unfold_umg(R, RF, ds, ts, eb, isos, name, program):
     U.set_ds(ds)
     U.set_routine(program)
     U.run(name)
-    plotit(U, ts, name)
+    # plotit(U, ts, name)
     shutil.rmtree('inp')
-    return U.solution, 100 * abs(U.solution.values - ts.values)/ ts.values
+    return U.solution, 100 * abs(U.solution.values - ts.values) / ts.values
 
 
 def plotit(U, ts, name):
     # load dataset
     sol = U.solution
-        
+
     fig = plt.figure(123)
     ax = fig.add_subplot(111)
     ax.set_xscale('log')
     ax.set_yscale('log')
     ax.set_xlabel('E (MeV)')
     ax.set_ylabel('$\Phi$')
-    
+
     ax.plot(*U.ds.step, color='k', label='Default')
     ax.plot(*ts.step, color='r', linestyle='--', label='True')
-    ax.plot(*sol.step, color='b',linestyle=':', label='MAXED')
-    
+    ax.plot(*sol.step, color='b', linestyle=':', label='MAXED')
+
     ax.legend(frameon=False)
     fig.savefig(plot_path + name + '.pdf')
     plt.close(fig)
-    
+
     # plot comparisons
     fig = plt.figure(124)
     ax = fig.add_subplot(111)
     ax.set_xlabel('Group')
     ax.set_ylabel('$\Phi_{MAXED}/$\Phi_{true}$')
-    
+
     x = range(U.ds.num_bins)
-    y = abs(sol.values - ts.values) / ts.values
+    y = 100 * abs(sol.values - ts.values) / ts.values
     y = y[::-1]
     ax.plot(x, y, color='k', marker='o')
     fig.savefig(plot_path + name + '_comp.pdf')
     plt.close(fig)
-    
+
     os.system('rm ' + data_path + '{}_unfolded.txt'.format(name))
-    
+
 
 def bin_flux(flux, struct):
     lower = 1e-5
     upper = 2e7
     eb = energy_groups(struct, lower, upper)
     phi = sp.zeros(len(eb)-1)
-    for i in range(len(eb)-1) :
+    for i in range(len(eb)-1):
         E = sp.logspace(sp.log10(eb[i+1]), sp.log10(eb[i]), 1e4)
         x = trapz(flux(E), E)
-        print("->",i,eb[i+1], eb[i], x)
+        print("->", i, eb[i+1], eb[i], x)
         phi[i] = trapz(flux(E), E)
     phi = phi[::-1] / np.sum(phi)
     return phi
@@ -96,47 +93,47 @@ isos_gd = ['u233gd155', 'u235gd155', 'pu238gd155', 'pu239gd155', 'pu241gd155']
 
 isos_1 = ['u235', 'u238', 'th232']
 isos_2 = ['u235', 'u238', 'th232', 'np237', 'pu238']
-isos_3 = ['th232','u233','u234','u235','u238','np237',
-          'pu238','pu239','pu240','pu241','pu242']
+isos_3 = ['th232', 'u233', 'u234', 'u235', 'u238', 'np237',
+          'pu238', 'pu239', 'pu240', 'pu241', 'pu242']
 isos_4 = isos
 isos_5 = isos_3 + isos_cd
 isos_6 = isos_3 + isos_gd
-isos_7 = isos_3 + isos_cd + isos_gd  
+isos_7 = isos_3 + isos_cd + isos_gd
 
 structs = ['tg0_625', 'lwr32', 'wims69']
-solution_data = {}
+umg_data = {}
 for struct in structs:
     phi_triga = select_flux_spectrum('trigaC', 1)[2]
     name = 'test_wims69_resp.p'
-    resp = generate_responses(isos+isos_cd+isos_gd, phi_triga, 
+    resp = generate_responses(isos+isos_cd+isos_gd, phi_triga,
                               struct=struct, name=name, overwrite=True)
     R = integral_response(name)
     RF = resp['response']
     for key, val in RF.items():
         RF[key] = val[::-1]
-    
+
     phi_ref = resp['phi'][::-1]
     phi_ref = phi_ref / sum(phi_ref)
     eb = resp['eb'][::-1] * 1e-6  # convert to MeV
     ts = Spectrum(eb, phi_ref)
-    
+
     pwr = Flux(7.0, 600.0)
     ds_pwr = bin_flux(pwr.evaluate, struct)
     ds_low = Spectrum(eb, ds_pwr * 0.5)
     ds_hi = Spectrum(eb, ds_pwr * 2)
     ds_on = Spectrum(eb, ds_pwr)
+    ds_ones = Spectrum(eb, ds_pwr * 0 + 1)
     default_spectra = {'hi': ds_hi,
                        'low': ds_low,
-                       'on': ds_on}
-    
+                       'on': ds_on,
+                       'ones': ds_ones}
+
     all_iso_sets = [isos_1, isos_2, isos_3, isos_4, isos_5, isos_6, isos_7]
     for i, iso_set in enumerate(all_iso_sets):
         for key, ds in default_spectra.items():
             for p, pgm in [('mx', 'maxed'), ('gr', 'gravel')]:
                 nombre = '{}_{}_{}_iso{}'.format(struct, key, p, i+1)
-                solution_data[nombre] = unfold_umg(R, RF, ds, ts, eb, isos=iso_set, name=nombre, program=pgm)
+                umg_data[nombre] = unfold_umg(R, RF, ds, ts, eb, isos=iso_set, name=nombre, program=pgm)
 
-for key, val in solution_data.items():
-    print('{}:  {}'.format(key, sp.mean(val[1])))
-
-
+with open(data_path + 'umg_data.p', 'wb') as F:
+    pickle.dump(umg_data, F)
