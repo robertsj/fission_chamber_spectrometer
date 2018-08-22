9ab0759137f3dcfb2144357c2ef9835b746441b4
John Boyington <mjcb@ksu.edu>
2018-08-22 13:30:52 -0500

    added plotting for isos and shielded responses

--- code/process_umg.py
+++ code/process_umg.py
@@ -25,7 +25,7 @@ def make_ds_plot(umg_data, pgm):
     # make style iterables
     colors = ['green', 'red', 'blue', 'orange', 'black']
     linestyles = ['-', '-.', ':', '--', '-']
-    markers = ['o', 's', '^', '*']
+    markers = ['s', 'o', '^', '*']
 
     # FOR DEFAULT SPECTRA
     # setup plotting environment
@@ -88,5 +88,118 @@ def make_ds_plot(umg_data, pgm):
     fig.clf()
 
 
-make_ds_plot(umg_data, 'mx')
-make_ds_plot(umg_data, 'gr')
+def make_isos_plot(umg_data, pgm):
+    ts = umg_data['wims69_ones_gr_iso4'][2]
+    # grab solutions
+    sol = {}
+    sol_err = {}
+    for lab, spec in [('Case 1', 'wims69_on_{}_iso1'.format(pgm)),
+                      ('Case 2', 'wims69_on_{}_iso2'.format(pgm)),
+                      ('Case 3', 'wims69_on_{}_iso3'.format(pgm)),
+                      ('Case 4', 'wims69_on_{}_iso4'.format(pgm))]:
+        sol[lab] = umg_data[spec][0].solution
+        sol_err[lab] = umg_data[spec][1]
+
+    # make style iterables
+    colors = ['green', 'red', 'blue', 'orange']
+    linestyles = ['-', '-.', ':', '--']
+    markers = ['s', 'o', '^', '*']
+
+    # setup plotting environment
+    fig = plt.figure(1235)
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('E $MeV$')
+    ax.set_ylabel('$\Phi$')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    # plot spectra
+    ax.plot(*ts.step, label='True', linestyle='-', linewidth=1.0, color='k')
+    for i, val in enumerate(sol.items()):
+        lab, spec = val
+        ax.plot(*spec.step, label=lab, linestyle=linestyles[i], linewidth=1.0, color=colors[i])
+
+    ax.legend(frameon=False)
+    fig.savefig(plot_path + 'umg_isos_{}.pdf'.format(pgm))
+    fig.clf()
+
+    # FOR DEFAULT SPECTRA SOLUTION ERRORS
+    # setup plotting environment
+    fig = plt.figure(1236)
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('g')
+    ax.set_ylabel('%')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    for i, val in enumerate(sol_err.items()):
+        lab, err = val
+        ax.plot(range(len(err)), err, label=lab + ' ({:4.1f})'.format(sp.mean(err)),
+                linestyle=linestyles[i], linewidth=1.0, color=colors[i], marker=markers[i])
+
+    ax.legend(frameon=False)
+    fig.savefig(plot_path + 'umg_isos_err_{}.pdf'.format(pgm))
+    fig.clf()
+
+
+def make_shielded_plot(umg_data, pgm):
+    ts = umg_data['wims69_ones_gr_iso4'][2]
+    # grab solutions
+    sol = {}
+    sol_err = {}
+    for lab, spec in [('Case 3', 'wims69_on_{}_iso3'.format(pgm)),
+                      ('Case 3 + Cd', 'wims69_on_{}_iso5'.format(pgm)),
+                      ('Case 3 + Gd', 'wims69_on_{}_iso6'.format(pgm)),
+                      ('Case 3 + Cd and Gd', 'wims69_on_{}_iso7'.format(pgm))]:
+        sol[lab] = umg_data[spec][0].solution
+        sol_err[lab] = umg_data[spec][1]
+
+    # make style iterables
+    colors = ['green', 'red', 'blue', 'orange']
+    linestyles = ['-', '-.', ':', '--']
+    markers = ['s', 'o', '^', '*']
+
+    # setup plotting environment
+    fig = plt.figure(1235)
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('E $MeV$')
+    ax.set_ylabel('$\Phi$')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    # plot spectra
+    ax.plot(*ts.step, label='True', linestyle='-', linewidth=1.0, color='k')
+    for i, val in enumerate(sol.items()):
+        lab, spec = val
+        ax.plot(*spec.step, label=lab, linestyle=linestyles[i], linewidth=1.0, color=colors[i])
+
+    ax.legend(frameon=False)
+    fig.savefig(plot_path + 'umg_shielded_{}.pdf'.format(pgm))
+    fig.clf()
+
+    # FOR DEFAULT SPECTRA SOLUTION ERRORS
+    # setup plotting environment
+    fig = plt.figure(1236)
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('g')
+    ax.set_ylabel('%')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    for i, val in enumerate(sol_err.items()):
+        lab, err = val
+        ax.plot(range(len(err)), err, label=lab + ' ({:4.1f})'.format(sp.mean(err)),
+                linestyle=linestyles[i], linewidth=1.0, color=colors[i], marker=markers[i])
+
+    ax.legend(frameon=False)
+    fig.savefig(plot_path + 'umg_shielded_err_{}.pdf'.format(pgm))
+    fig.clf()
+
+for pgm in ['mx', 'gr']:
+    #make_ds_plot(umg_data, pgm)
+    #make_isos_plot(umg_data, pgm)
+    make_shielded_plot(umg_data, pgm)
